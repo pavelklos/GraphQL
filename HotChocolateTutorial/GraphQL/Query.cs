38278c56c9fe4944190165265d275580bed4ec51
Pavel Klos <pavelkloscz@gmail.com>
2023-06-19 20:44:27 +0200

    Code from NDC Copenhagen 2022

--- HotChocolateTutorial/GraphQL/Query.cs
+++ HotChocolateTutorial/GraphQL/Query.cs
@@ -20,8 +20,24 @@ public class Query
     public IEnumerable<User> GetUsers() => _repository.Users;
     public User[] GetUsersByRole(UserRole role) =>
         _repository.Users.Where(u => u.Role == role).ToArray();
+
     // Other
     public DateTime GetDateNow() => DateTime.Now; // "2023-06-12T13:21:44.096+02:00"
     public int GetRandom() => new Random().Next();
     public Guid GetGuid() => Guid.NewGuid();
+
+    // NDC Copenhagen 2022
+    // (Michael Staib)
+    // - Getting started with GraphQL in .NET
+    public string Hello(string name = "World") =>
+        $"Hello, {name}!";
+    public record AuthorNDC( string Name );
+    public record BookNDC( string Title, AuthorNDC Author );
+    public IEnumerable<BookNDC> GetBooksNDC()
+    {
+        var author = new AuthorNDC("Jon Skeet");
+        yield return new BookNDC("C# in Depth", author);
+        yield return new BookNDC("C# in Depth 2nd Edition", author);
+    }
+
 }
